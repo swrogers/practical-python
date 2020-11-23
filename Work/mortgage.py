17fa78b82c54dc9cb9d700c740025ffd2a6f9ee0
Shane Rogers <swrogers@gmail.com>
2020-11-23 14:34:43 -0600

    mortgage.py - additional payments over x months

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -9,13 +9,20 @@ total_paid = 0.0
 months = 0
 
 extra_to_prin = 1000.0
-extra_pay_len = 12
+extra_payment_start_month = 12 * 5  # start in year 5
+extra_payment_end_month = (12 * 5) + (4*12) # pay for 4 years
+extra_pay_len = extra_payment_end_month - extra_payment_start_month + 1
 
 while principal > 0:
     months = months + 1
     payment = set_payment
+
+    #print(months, round(principal,2))
+    print(f'{months:2} : {principal:0.2f}')
     
-    if extra_pay_len:
+    if (extra_pay_len
+        and months >= extra_payment_start_month
+        and months <= extra_payment_end_month):
         payment = payment + extra_to_prin
         extra_pay_len = extra_pay_len - 1
     
